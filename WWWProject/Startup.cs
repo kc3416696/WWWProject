efebb2678e57c910cae2d4f17131f639ba0a1c8b
yuan <kc3416696@gmail.com>
2017-03-27 17:59:36 +0800

    加入專案檔案。

---
+++ WWWProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WWWProject.Startup))]
+namespace WWWProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
